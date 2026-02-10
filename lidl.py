9ed5c5db85359f4f5b49ce39f4bb7a67e953926d
rzabeyda <131679219+rzabeyda@users.noreply.github.com>
2026-02-10 12:55:07 +0200

    Update lidl.py

--- lidl.py
+++ lidl.py
@@ -2,7 +2,6 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-print("===== Запущена версия бота 1.0 =====")
 TOKEN = "8329451130:AAFUUW8G45LY2mobMo9yK33cnTbEnN38lY8"
 URL = "https://rzabeyda.github.io/Lidl/"
 
@@ -15,4 +14,4 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
-    print("Бот запущен! Версия 1")
+    print("Бот запущен!")
