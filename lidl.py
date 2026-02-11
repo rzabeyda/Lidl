9d53ad4a60e2734ee8fcd87b2b1019500a669f3f
Roman <rzabeyda@gmail.com>
2026-02-11 11:49:50 +0200

    Pics and Greetings

--- lidl.py
+++ lidl.py
@@ -1,19 +1,30 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, InputFile
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("BOT_TOKEN не найден в переменных окружения")
+
+# Ссылка на веб-шоп
 URL = "https://rzabeyda.github.io/Lidl/"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("Открыть", url=URL)]]
+    user_name = update.effective_user.first_name
+    text = f"Привет, {user_name} 🤗"
+
+    keyboard = [[InlineKeyboardButton("Зашопиться 🛍️", url=URL)]]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text("Привет!", reply_markup=reply_markup)
+
+    # Отправляем картинку + текст + кнопку
+    with open("lidl.png", "rb") as f:  # путь к твоей картинке
+        await update.message.reply_photo(photo=InputFile(f), caption=text, reply_markup=reply_markup)
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    app.run_polling()
     print("Бот запущен!")
+    try:
+        app.run_polling()
+    except KeyboardInterrupt:
+        print("Всё ровно!")
